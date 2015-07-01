496178783fe0d78784a48d32ced9fa3ecf846ed2
yass <yasser.moukhtar@gmail.com>
2015-07-01 14:30:54 +0200

    Added Multer

--- app/routes/articles.server.routes.js
+++ app/routes/articles.server.routes.js
@@ -5,12 +5,20 @@
  */
 var users = require('../../app/controllers/users'),
 	articles = require('../../app/controllers/articles');
+var multer =require('multer');
+var fileHandler =require('../../app/controllers/local-file-handle');
+var multerTemp=multer({ dest: config.filesTemp, limits: {
+	fieldNameSize: 255,
+	fields: 7,
+	files: 1,
+	fileSize:100000
+}});
 
 module.exports = function(app) {
 	// Article Routes
 	app.route('/articles')
 		.get(articles.list)
-		.post(users.requiresLogin, articles.create);
+		.post(users.requiresLogin, multerTemp, articles.create, fileHandler.uploadFile);
 
 	app.route('/articles/:articleId')
 		.get(articles.read)
